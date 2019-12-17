92203ac011230b890706306041358c1277860dfb
Chaoqian Xu <chaoranxu@gmail.com>
2019-12-17 20:57:58 +0800

    typo

--- src/options.rs
+++ src/options.rs
@@ -12,24 +12,47 @@ impl Options {
         }
     }
 
+    pub fn set_table_cache_num_shard_bits(&mut self, num: i32) {
+        self.inner.set_table_cache_num_shard_bits(num);
+    }
+
     pub fn set_write_buffer_size(&mut self, size: usize) {
         self.inner.set_write_buffer_size(size);
     }
 
-    pub fn set_max_write_buffer_number(&mut self, size: i32) {
-        self.inner.set_max_write_buffer_number(size);
+    pub fn set_max_write_buffer_number(&mut self, num: i32) {
+        self.inner.set_max_write_buffer_number(num);
+    }
+    pub fn set_min_write_buffer_number_to_merge(&mut self, num: i32) {
+        self.inner.set_min_write_buffer_number_to_merge(num);
     }
 
-    pub fn set_min_write_buffer_number_to_merge(&mut self, size: i32) {
-        self.inner.set_min_write_buffer_number_to_merge(size);
+    pub fn set_max_bytes_for_level_base(&mut self, bytes: u64) {
+        self.inner.set_max_bytes_for_level_base(bytes);
     }
 
-    pub fn set_max_background_compactions(&mut self, size: i32) {
-        self.inner.set_max_background_compactions(size);
+    pub fn set_max_bytes_for_level_multiplier(&mut self, bytes: f64) {
+        self.inner.set_max_bytes_for_level_multiplier(bytes);
     }
 
-    pub fn set_max_background_flushes(&mut self, size: i32) {
-        self.inner.set_max_background_flushes(size);
+    pub fn set_target_file_size_base(&mut self, size: u64) {
+        self.inner.set_target_file_size_base(size);
+    }
+
+    pub fn set_target_file_size_multiplier(&mut self, size: i32) {
+        self.inner.set_target_file_size_multiplier(size);
+    }
+
+    pub fn set_level_zero_file_num_compaction_trigger(&mut self, num: i32) {
+        self.inner.set_level_zero_file_num_compaction_trigger(num);
+    }
+
+    pub fn set_max_background_compactions(&mut self, num: i32) {
+        self.inner.set_max_background_compactions(num);
+    }
+
+    pub fn set_max_background_flushes(&mut self, num: i32) {
+        self.inner.set_max_background_flushes(num);
     }
 
     fn build_default_options() -> InnerOptions {
